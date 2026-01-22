8eba0d2dbda3ebeca22c1fc1a8c6c70f2996b76b
muhammadoby <labyqulumam@gmail.com>
2026-01-22 17:34:12 +0800

    add ai schema

--- start/test_routes.ts
+++ start/test_routes.ts
@@ -1,15 +1,15 @@
 import router from "@adonisjs/core/services/router";
 import { HttpContext } from "@adonisjs/core/http";
 import BaseMessage from "../app/utils/base_message.js";
-import User from "#models/user";
+import AffBalance from "#models/aff_balance";
 
-router.get('users', async ({ request, response }: HttpContext) => {
+router.get('test', async ({ request, response }: HttpContext) => {
   const page = request.input('page', 1)
 
   try {
-    const user = await User.query().paginate(page, Number(10))
+    const user = await AffBalance.query().paginate(page, Number(10))
 
-    return response.status(200).send(BaseMessage(true, "Users fetched successfully", user))
+    return response.status(200).send(BaseMessage(true, "Data fetched successfully", user))
   } catch (error) {
     return response.status(error.status || 500).send(BaseMessage(false, error.message))
   }
